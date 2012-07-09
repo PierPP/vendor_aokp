cb00c8107dcf952b9b35d0de77ac85164364b226
PieR <pierpaolo@gmail.com>
2012-07-09 08:11:17 +0200

    Milestone 6
    Change-Id: Id4c322f645ad680ee093b6cc3923740af8efe84d

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -4,7 +4,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=relea
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.rommanager.developerid=teamkang \
-    ro.aokp.version=$(TARGET_PRODUCT)_build-40_pier_$(shell date +"%Y%m%d")
+    ro.aokp.version=$(TARGET_PRODUCT)_M6_pier_$(shell date +"%Y%m%d")
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
