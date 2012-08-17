63df6390ba6ed808966fb1d5e7b001047b50b3e2
Pierpaolo Pernici <pierpaolo@gmail.com>
2012-08-17 14:25:17 +0200

    Realigned to upstream (hand made Oo)
    Change-Id: I56db82bd24c50e81e25705accdd833256d81203c

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,10 +1,12 @@
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JRO03C BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JRO03H BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
+
+DATE = $(shell date +%h-%d-%y)
 
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.rommanager.developerid=teamkang \
-    ro.aokp.version=$(TARGET_PRODUCT)_jb_pier_$(shell date +"%Y%m%d")
+    ro.aokp.version=$(TARGET_PRODUCT)_jb_pier-$(DATE)
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
